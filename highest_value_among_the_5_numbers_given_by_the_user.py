8b2e986fbb9288eb1654c25827fa0587b260eebe
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-16 01:06:22 +0800

    Applying the same principle I implemented for my main algorithm; I able to create also an identifier for the number itself that contains the highest value (printing it altogether with the highest value itself). It is by assigning a variable for a string that describes that specific number (obviously an identifier), and that gist are running the same as I mentioned which is together with the main algotihm.

--- highest_value_among_the_5_numbers_given_by_the_user.py
+++ highest_value_among_the_5_numbers_given_by_the_user.py
@@ -54,27 +54,38 @@ while True:
         print("Please try again.\n")
                   
 #var for the assumed initial/current highest value = 1st Number
+#var for the assumed initial/current number with the highest value = 1st Number(string)
 highest_value = first_number
+number_with_the_highest_value = 'first number'
+
 
 #if the 2nd Number is > the current highest value
      #then the current highest value is now the 2nd Number
+     #then the current number with the highest value is now the 2nd Number(string)
 if second_number > highest_value:
      highest_value = second_number
+     number_with_the_highest_value = 'second number'
 
 #if the 3rd Number is > the current/latest highest value
      #then the current/latest highest value is now the 3rd Number
+     #then the current/latest number with the highest value is now the 3rd Number(string)
 if third_number > highest_value:
-     highest_value = third_number      
+     highest_value = third_number     
+     number_with_the_highest_value = 'third number' 
      
 #if the 4th Number is > the current/latest highest value
      #then the current/latest highest value is now the 4th Number
+     #then the current/latest number with the highest value is now the 4th Number(string)
 if fourth_number > highest_value:
      highest_value = fourth_number
+     number_with_the_highest_value = 'fourth number'
 
 #if the 5th Number is > the current/latest highest value
      #then the current/latest highest value is now the 5th Number
+     #then the current/latest number with the highest value is now the 5th Number(string)
 if fifth_number > highest_value:
-     highest_value = fifth_number           
+     highest_value = fifth_number  
+     number_with_the_highest_value = 'fifth number'      
 
-#print the var containing the highest value 
-print(f'\nThe highest value: {highest_value}\n')
+#print the var containing the number with the highest value together with the var containing the highest value itself
+print(f'\nThe {number_with_the_highest_value} have the highest value: {highest_value}\n')
