f87e4aab3374df5795bbb7e7c1d8aeeb5f4b624d
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-15 18:46:54 +0800

    Pseudocode of my program, which follows the source code format that I will code. I'm still planning to add something to it along the way as I code (just for modification purposes that I prefer to have in my source code).

---
+++ highest_value_among_the_5_numbers_given_by_the_user.py
@@ -0,0 +1,21 @@
+#input for the 1st num
+#input for the 2nd num
+#input for the 3rd num
+#input for the 4th num
+#input fot the 5th num
+
+#var for the max value
+#var for the number/s with the max value
+
+#if the max value is == to the 1st num
+     #then var for the number/s with the max value will bear the str '1st Number' together with the .append() method
+#if the max value is == to the 2nd num
+     #then var for the number/s with the max value will bear the str '2nd Number' together with the .append() method
+#if the max value is == to the 3rd num
+     #then var for the number/s with the max value will bear the str '3rd Number' together with the .append() method
+#if the max value is == to the 4th num
+     #then var for the number/s with the max value will bear the str '4th Number' together with the .append() method
+#if the max value is == to the 5th num
+     #then var for the number/s with the max value will bear the str '5th Number' together with the .append() method
+
+#print the var for the number/s with the max value together with the var for the max value
