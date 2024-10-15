d199cc13283d15a421e2969a048672b62bbf8c36
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-15 21:27:41 +0800

    I added some few modifications such as the title with a slant font from the library of pyfiglet which is figlet_format() and the new lines. As I work with the modifications, I noticed my variables are not aligned to the nature of the program which is finding the highest number, so I changed it to a declaration of 'highest', and there's one word in the string; so I also changed it.Now I'm officially done and happy about it.

--- highest_value_among_the_5_numbers_given_by_the_user.py
+++ highest_value_among_the_5_numbers_given_by_the_user.py
@@ -1,16 +1,21 @@
+import pyfiglet
+
+title = pyfiglet.figlet_format("\nHIGHEST NUMBER", font="slant")
+print(title)
+
 #input for the 1st num
 #input for the 2nd num
 #input for the 3rd num
 #input for the 4th num
 #input fot the 5th num
 while True:
-   first_number = input("Input the first number (any range): ")
+   first_number = input("\nInput the first number (any range): ")
    if first_number.isdigit():
         first_number = int(first_number)
         break
    else:
         print("Invalid input, must be a digit.")
-        print("Please try again.")  
+        print("Please try again.\n")  
 
 while True:         
    second_number = input("Input the second number (any range): ")
@@ -19,7 +24,7 @@ while True:
         break
    else:
         print("Invalid input, must be a digit.")
-        print("Please try again.")
+        print("Please try again.\n")
 
 while True:         
    third_number = input("Input the third number (any range): ")
@@ -28,7 +33,7 @@ while True:
         break
    else:
         print("Invalid input, must be a digit.")
-        print("Please try again.")    
+        print("Please try again.\n")    
 
 while True:         
    fourth_number = input("Input the fourth number (any range): ")
@@ -37,7 +42,7 @@ while True:
         break
    else:
         print("Invalid input, must be a digit.")
-        print("Please try again.")
+        print("Please try again.\n")
 
 while True:         
    fifth_number = input("Input the fifth number (any range): ")
@@ -46,12 +51,12 @@ while True:
         break
    else:
         print("Invalid input, must be a digit.")
-        print("Please try again.")
+        print("Please try again.\n")
                   
 #var for the max value
-maximum_value = max(first_number, second_number, third_number, fourth_number, fifth_number)
+highest_value = max(first_number, second_number, third_number, fourth_number, fifth_number)
 #var for the number/s with the max value
-number_or_numbers_with_the_maximum_value = []
+number_or_numbers_with_the_highest_value = []
 
 #if the max value is == to the 1st num
      #then var for the number/s with the max value will bear the str '1st Number' together with the .append() method
@@ -63,16 +68,16 @@ number_or_numbers_with_the_maximum_value = []
      #then var for the number/s with the max value will bear the str '4th Number' together with the .append() method
 #if the max value is == to the 5th num
      #then var for the number/s with the max value will bear the str '5th Number' together with the .append() method
-if maximum_value == first_number:
-    number_or_numbers_with_the_maximum_value.append('1st Number')
-if maximum_value == second_number:
-    number_or_numbers_with_the_maximum_value.append('2nd Number')
-if maximum_value == third_number:
-    number_or_numbers_with_the_maximum_value.append('3rd Number')
-if maximum_value == fourth_number:
-    number_or_numbers_with_the_maximum_value.append('4th Number')
-if maximum_value == fifth_number:
-    number_or_numbers_with_the_maximum_value.append('5th Number')   
+if highest_value == first_number:
+    number_or_numbers_with_the_highest_value.append('1st Number')
+if highest_value == second_number:
+    number_or_numbers_with_the_highest_value.append('2nd Number')
+if highest_value == third_number:
+    number_or_numbers_with_the_highest_value.append('3rd Number')
+if highest_value == fourth_number:
+    number_or_numbers_with_the_highest_value.append('4th Number')
+if highest_value == fifth_number:
+    number_or_numbers_with_the_highest_value.append('5th Number')   
 
 #print the var for the number/s with the max value together with the var for the max value
-print(f'The {", ".join(number_or_numbers_with_the_maximum_value)} have the maximum value: {maximum_value}')
+print(f'\nThe {", ".join(number_or_numbers_with_the_highest_value)} have the highest value: {highest_value}\n')
