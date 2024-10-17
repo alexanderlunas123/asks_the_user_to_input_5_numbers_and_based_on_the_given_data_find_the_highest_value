a719d44e8315535e9b63419378cf65f9c80da5d8
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-17 22:24:17 +0800

    Added the elif statements in order to address the special case when there's more than one number that shares the highest value given by the inputted values of the user on each specific numbers.

--- highest_value_among_the_5_numbers_given_by_the_user.py
+++ highest_value_among_the_5_numbers_given_by_the_user.py
@@ -54,38 +54,59 @@ while True:
         print("Please try again.\n")
                   
 #var for the assumed initial/current highest value = 1st Number
-#var for the assumed initial/current number with the highest value = 1st Number(string)
+#var assigning a list for the current/latest number(s) with the highest value 
 highest_value = first_number
-number_with_the_highest_value = 'first number'
+number_with_the_highest_value = []
 
+#if the var for the assumed initial/current highest value is indeed the 1st Number
+     #then 'first number' (string) will be appended to the var assigning a list for the current/latest number(s) with the highest value
+if highest_value == first_number:
+     number_with_the_highest_value.append('"first number"')
 
-#if the 2nd Number is > the assumed initial/current highest value
+#if the 2nd Number is > the current highest value
      #then the current/latest highest value is now the 2nd Number
-     #then the current/latest number with the highest value is now the 2nd Number(string)
+     #then 'second number' (string) will be added and reset the previous possible executions to the var assigning a list for the current/latest number(s) with the highest value
+#elif the 2nd Number does indeed shares the same value as the current highest value   
+     #then 'second_number' (string) will be appended to the var assigning a list for the current/latest number(s) with the highest value
 if second_number > highest_value:
      highest_value = second_number
-     number_with_the_highest_value = 'second number'
+     number_with_the_highest_value = ['"second number"']
+elif second_number == highest_value:
+     number_with_the_highest_value.append('"second number"')
 
 #if the 3rd Number is > the current/latest highest value
      #then the current/latest highest value is now the 3rd Number
-     #then the current/latest number with the highest value is now the 3rd Number(string)
+     #then 'third number' (string) will be added and reset the previous possible executions to the var assigning a list for the current/latest number(s) with the highest value
+#elif the 3rd Number does indeed shares the same value as the current/latest highest value       
+     #then 'third number' (string) will be appended to the var assigning a list for the current/latest number(s) with the highest value
 if third_number > highest_value:
-     highest_value = third_number     
-     number_with_the_highest_value = 'third number' 
+     highest_value = third_number 
+     number_with_the_highest_value = ['"third number"']
+elif third_number == highest_value:         
+     number_with_the_highest_value.append('"third number"') 
      
 #if the 4th Number is > the current/latest highest value
      #then the current/latest highest value is now the 4th Number
-     #then the current/latest number with the highest value is now the 4th Number(string)
+     #then 'fourth number' (string) will be added and reset the previous possible executions to the var assigning a list for the current/latest number(s) with the highest value 
+#elif the 4th Number does indeed shares the same value as the current/latest highest value    
+     #then 'fourth number' (string) will be appended to the var assigning a list for the current/latest number(s) with the highest value
 if fourth_number > highest_value:
      highest_value = fourth_number
-     number_with_the_highest_value = 'fourth number'
+     number_with_the_highest_value = ['"fourth number"']
+elif fourth_number == highest_value:     
+     number_with_the_highest_value.append('"fourth number"')
 
 #if the 5th Number is > the current/latest highest value
      #then the current/latest highest value is now the 5th Number
-     #then the current/latest number with the highest value is now the 5th Number(string)
+     #then 'fifth number' (string) will be added and reset the previous possible executions to the var assigning a list for the current/latest number(s) with the highest value
+#elif the 5th Number does indeed shares the same value as the current/latest highest value     
+     #then 'fifth number' (string) will be appended to the var assigning a list for the current/latest number(s) with the highest value
 if fifth_number > highest_value:
      highest_value = fifth_number  
-     number_with_the_highest_value = 'fifth number'      
+     number_with_the_highest_value = ['"fifth number"']
+elif fifth_number == highest_value:  
+     number_with_the_highest_value.append('"fifth number"')      
 
-#print the var containing the number with the highest value together with the var containing the highest value itself
-print(f'\nThe "{number_with_the_highest_value}" have the highest value: {highest_value}\n')
+#print the var assigning a list for the current/latest number(s) with the highest value together with the var containing the current/latest highest value itself
+print(f'\nThe {"; ".join(number_with_the_highest_value)} have the highest value: {highest_value}\n')
+
